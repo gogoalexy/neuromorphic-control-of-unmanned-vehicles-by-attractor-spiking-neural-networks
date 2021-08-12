a3ce040571fe5b548eb75b527a3bea8b79f29c31
Alessio Yao <huangyu_yao@lolab-nthu.org>
2021-08-13 04:06:06 +0800

    Add result of CoS weight scan

--- cos.py
+++ cos.py
@@ -1,28 +1,37 @@
 import argparse
 
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
 from ssm import SNNStateMachine
 
 if __name__ == '__main__':
 
-    for weight in np.logspace(0.1, 10, 10):
-        results = []
-        for strength in range(100, 1000, 100):
-            row = []
-            for duration in range(50, 500, 50):
-                ssc = SNNStateMachine(2, transitions=1, task_weights=[weight, weight], experiment_time=1000, repetition=1)
-                ssc.setTransitionPeriod(500, duration, 500)
-                ssc.generateTransitionStimuli('spike', 'AMPA', strength)
-                ssc.spawnAttractor(100, 50, 'spike', 'AMPA', 400)
-                ssc.startSimulation()
-                x0, x1 = ssc.calculateRobustness()
-                row.append(x1)
-                #ssc.plotRaster()
-            results.insert(0, row)
+    duration = 150
+    results = []
+    #for weight in np.logspace(0.1, 10, 10):
+    for strength in range(50, 800, 50):
+        row = []
+        for weight in range(1, 10, 1):
+            ssc = SNNStateMachine(2, transitions=1, task_weights=[weight, weight], experiment_time=1000, repetition=100)
+            ssc.setTransitionPeriod(500, duration, 500)
+            ssc.generateTransitionStimuli('spike', 'AMPA', float(strength))
+            ssc.spawnAttractor(100, 50, 'spike', 'AMPA', 400)
+            ssc.startSimulation()
+            x0, x1 = ssc.calculateRobustness()
+            row.append(x1)
+            #ssc.plotRaster()
+        results.insert(0, row)
 
-        fig = plt.figure()
-        ax = fig.add_subplot()
-        im = ax.imshow(np.array(results))
-        plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    cbar = fig.colorbar(cm.ScalarMappable(norm=None, cmap='inferno'), ax=ax)
+    cbar.ax.get_yaxis().labelpad = 15
+    cbar.ax.set_ylabel('Robustness', rotation=270)
+    im = ax.imshow(np.array(results), cmap='inferno', extent=[0.5,9.5,25,775], aspect='auto')
+    plt.xlabel('Task weight')
+    plt.ylabel("Stimulus strength to 'Status' (Hz)")
+    plt.xticks([float(i) for i in range(1, 10, 1)])
+    plt.yticks([i for i in range(50, 800, 50)])
+    plt.show()
